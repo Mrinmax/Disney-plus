041555f231f31daa0538f0e90c5e32fbb68a6826
Mrinal kanti sardar <64627885+Mrinmax@users.noreply.github.com>
2021-12-16 17:27:30 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Redux</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
