2c60e1b81e569f90e25c176fb148a4ed3c879c7f
Mrinal kanti sardar <64627885+Mrinmax@users.noreply.github.com>
2021-12-17 18:36:52 +0530

    Login done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,12 +12,16 @@ import {
   Link
 } from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
